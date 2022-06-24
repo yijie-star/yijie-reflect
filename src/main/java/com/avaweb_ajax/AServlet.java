987d24f9843a1e9eb9dda544ee17615a89340d2c
yijie <smile_yijie@163.com>
2022-06-24 13:40:13 +0800

    yijie-git

---
+++ src/main/java/com/avaweb_ajax/AServlet.java
@@ -0,0 +1,14 @@
+package com.avaweb_ajax;
+
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println("Hello Ajax!");
+        response.getWriter().print("Hello Ajax!!!!!");
+    }
+}
